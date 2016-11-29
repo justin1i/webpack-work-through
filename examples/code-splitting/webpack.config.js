11200165b7c0aa8bfbef647de7d3d74cf879f405
justin1i <justin1i@126.com>
2016-11-29 17:19:05 +0800

    fixed

--- examples/code-splitting/webpack.config.js
+++ examples/code-splitting/webpack.config.js
@@ -1,9 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: {
-    bundle: path.join(__dirname, 'src'),
-  },
+  entry: path.join(__dirname, 'src'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].[chunkhash].js',
